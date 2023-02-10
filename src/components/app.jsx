ecba1501ef032792e7effc0367333c600cc1df3a
Dal Robinson <dalwrobinson@protonmail.com>
2023-02-10 11:36:27 +0000

    last commit course is out of date

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,11 +1,27 @@
 import React, { Component } from "react";
 
+import SearchBar from './search_bar';
+import Gif from './gif';
+import GifList from './gif_list';
+
 class App extends Component {
-  render() {
+    render() {
+      const gifs = [
+        { id: "xvhkuAxYLav60x50oF" },
+        { id: "02muhNyezC8QcuWhFX" }
+      ];
     return (
       <div>
-        <div className="left-scene"></div>
-        <div className="right-scene"></div>
+        <div className="left-scene">
+          <SearchBar />
+          <div className="selected-gif">
+            <Gif id="xvhkuAxYLav60x50oF" />
+          </div>
+        </div>
+        <div className="right-scene">
+          <GifList gifs={gifs} />
+        </div>
+        .red
       </div>
     );
   }
